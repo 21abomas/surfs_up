4f00bbff74dbf359256b045e267824b53022e7ca
21abomas <samuelaboma2002@gmail.com>
2023-03-23 03:30:12 -0400

    updating

--- app.py
+++ app.py
@@ -1,8 +1,94 @@
-# Set up the Flask Weather App
-# Import dependencies
-from flask import Flask
+# import dependencies
+import datetime as dt
+import numpy as np
+import pandas as pd
+import sqlalchemy
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.orm import Session
+from sqlalchemy import create_engine, func
+from flask import Flask, jsonify
+
+# set up database engine
+engine = create_engine("sqlite:///hawaii.sqlite")
+Base = automap_base()
+Base.prepare(engine, reflect=True)
+Measurement = Base.classes.measurement
+Station = Base.classes.station
+session = Session(engine)
+
+# set up Flask
 app = Flask(__name__)
 
-@app.route('/')
-def hello_world ():
-    return 'hello world'
+# define home page
+@app.route("/")
+def welcome():
+    return(
+    '''
+    Welcome to the Surf's Up API!<br>
+    Available Routes:<br>
+    /api/v1.0/precipitation<br>
+    /api/v1.0/stations<br>
+    /api/v1.0/tobs<br>
+    /api/v1.0/start<br>
+    /api/v1.0/start/end
+    ''')
+
+# 9.5.2 says to use flask run
+
+# 9.5.3 Precipitation Route
+def precipitation():
+   prev_year = dt.date(2017, 8, 23) - dt.timedelta(days=365)
+   precipitation = session.query(Measurement.date, Measurement.prcp).\
+    filter(Measurement.date >= prev_year).all()
+   precip = {date: prcp for date, prcp in precipitation}
+   return jsonify(precip)
+
+# 9.5.4 Stations Route
+@app.route("/api/v1.0/stations")
+def stations():
+    results = session.query(Station.station).all()
+    stations = list(np.ravel(results))
+    return jsonify(stations=stations)
+
+#9.5.5 Monthly Temperature Route
+@app.route("/api/v1.0/tobs")
+
+def temp_monthly():
+    prev_year = dt.date(2017, 8, 23) - dt.timedelta(days=365)
+    results = session.query(Measurement.tobs).\
+      filter(Measurement.station == 'USC00519281').\
+      filter(Measurement.date >= prev_year).all()
+    temps = list(np.ravel(results))
+    return jsonify(temps=temps)
+#do flask run, (http://localhost:5000/), block of temps (F)
+
+#9.5.6 Statistic Route
+@app.route("/api/v1.0/temp/<start>")
+@app.route("/api/v1.0/temp/<start>/<end>")
+
+def stats(start=None, end=None):
+    sel = [func.min(Measurement.tobs), func.avg(Measurement.tobs), func.max(Measurement.tobs)]
+    if not end:
+        results = session.query(*sel).\
+            filter(Measurement.date >= start).\
+            filter(Measurement.date <= end).all()
+        temps = list(np.ravel(results))
+        return jsonify(temps)
+    results = session.query(*sel).\
+        filter(Measurement.date >= start).\
+        filter(Measurement.date <= end).all()
+    temps = list(np.ravel(results))
+    return jsonify(temps=temps)
+# flask run
+# After running this code, you'll be able to copy and paste the web address provided by Flask into a web browser. 
+# Open /api/v1.0/temp/start/end route and check to make sure you get the correct result, which is
+# [null,null,null]
+# You would add the following path to the address in your web browser:
+# /api/v1.0/temp/2017-06-01/2017-06-30
+# should return 
+# ["temps":[71.0,77.21989528795811,83.0]]
+
+# from tech help
+# if __name__ == '__main__':
+#     app.run()
+
